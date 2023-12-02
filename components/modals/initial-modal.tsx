71af76a3a144a9db2fa89b622a9fc8ccb5cb3802
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-02 19:01:17 +0300

    ✨ feat(initial-modal.tsx): add InitialModal component to handle customization of new server
    The InitialModal component is added to handle the customization of a new server. It includes a form with fields for server name and server icon URL. The form is validated using the zod library. The component also includes a loading state and an onSubmit function to handle form submission.

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '../ui/dialog';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { useEffect, useState } from 'react';
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: 'Server name must be at least 2 characters long'
+    })
+    .max(100),
+  imageUrl: z
+    .string()
+    .min(2, {
+      message: 'Server icon URL must be at least 2 characters long'
+    })
+    .url({
+      message: 'Server icon URL must be a valid URL'
+    })
+});
+
+const InitialModal = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      imageUrl: ''
+    }
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Dialog open>
+      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your new server a personality with a name and an icon. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                TODO: Image Upload
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button disabled={isLoading} variant="primary">
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default InitialModal;
