ce3540a3bf56765f1848afec075f6270472dc51a
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-03 20:04:41 +0300

    🔧 chore(next.config.js): add remotePatterns configuration to images in next.config.js to allow loading images from specific remote hosts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'uploadthing.com', protocol: 'https' },
+      { hostname: 'utfs.io', protocol: 'https' }
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
