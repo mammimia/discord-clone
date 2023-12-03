98418b33b2c993f12de2b3c1cefad301e4503601
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-03 20:08:03 +0300

    🎨 style(file-upload.tsx): import and use 'X' icon from 'lucide-react' library for delete button 🔧 chore(file-upload.tsx): add delete button to allow users to remove uploaded file

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,6 +2,7 @@
 
 import { UploadDropzone } from '@/lib/uploadthing';
 import '@uploadthing/react/styles.css';
+import { X } from 'lucide-react';
 import Image from 'next/image';
 
 interface FileUploadProps {
@@ -17,6 +18,13 @@ const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
     return (
       <div className="relative h-20 w-20">
         <Image src={value} fill className="rounded-full" alt="Upload" />
+        <button
+          onClick={() => onChange()}
+          className="absolute right-0 top-0 rounded-full bg-rose-500 p-1
+           text-white shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     );
   }
