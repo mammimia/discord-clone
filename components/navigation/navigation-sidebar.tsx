7f3bf8b55913f70ed2b7d4be2b09b3dce6c9c9e9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-04 09:05:13 +0300

    🔧 fix(navigation-sidebar.tsx): import missing dependencies ScrollArea and Separator to resolve compilation errors 🚀 feat(navigation-sidebar.tsx): add dynamic rendering of NavigationItems based on servers data to improve navigation functionality and user experience

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,10 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Separator } from '@/components/ui/separator';
 import { getCurrentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirect } from 'next/navigation';
 import NavigationAction from './navigation-action';
+import { NavigationItem } from './navigation-item';
 
 const NavigationSidebar = async () => {
   const profile = await getCurrentProfile();
@@ -26,6 +29,21 @@ const NavigationSidebar = async () => {
     space-y-4 py-3 text-primary dark:bg-[#1E1F22]"
     >
       <NavigationAction />
+      <Separator
+        className="mx-auto h-[2px] w-10 rounded-md
+      bg-zinc-300 dark:bg-zinc-700"
+      />
+      <ScrollArea className="w-full flex-1">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
