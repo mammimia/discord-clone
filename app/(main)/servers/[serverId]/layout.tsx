ff802cd467c0747696ac130dd78c3c9733fc75b9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-04 10:56:45 +0300

    ✨ feat(layout.tsx): add ServerIdLayout component to handle rendering of server-specific layout 🐛 fix(layout.tsx): handle cases where user is not signed in by redirecting to sign-in page 🐛 fix(layout.tsx): handle cases where server with given serverId does not exist by redirecting to home page

---
+++ app/(main)/servers/[serverId]/layout.tsx
@@ -0,0 +1,45 @@
+import ServerSidebar from '@/components/server/server-sidebar';
+import { getCurrentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const ServerIdLayout = async ({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) => {
+  const profile = await getCurrentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  if (!server) {
+    return redirect('/');
+  }
+
+  return (
+    <div className="h-full">
+      <div className="z-60 fixed inset-y-0 hidden h-full w-60 flex-col md:flex">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
