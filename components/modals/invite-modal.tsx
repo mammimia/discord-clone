6fb620a443267b77454460579f8f3268844516b0
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-05 11:13:27 +0300

    🔧 fix(invite-modal.tsx): remove unused DialogDescription component to improve code readability ✨ feat(invite-modal.tsx): add functionality to copy server invite link and generate a new link for inviting friends to the server

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,32 +1,96 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle
 } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { useModal } from '@/hooks/use-modal-store';
+import { useOrigin } from '@/hooks/use-origin';
+import axios from 'axios';
+import { Check, Copy, RefreshCw } from 'lucide-react';
+import { useState } from 'react';
 
 const InviteModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onOpen, onClose, type, data } = useModal();
+  const origin = useOrigin();
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inviteUrl = `${origin}/invite/${data?.server?.inviteCode}`;
 
   const isModalOpen = type === 'invite' && isOpen;
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const onNewLink = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(
+        `/api/servers/${data?.server?.id}/invite-code`
+      );
+
+      onOpen('invite', { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="overflow-hidden bg-white p-0 text-black">
         <DialogHeader className="px-6 pt-8">
           <DialogTitle className="text-center text-2xl font-bold">
-            Customize your server
+            Invite Friends
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Give your new server a personality with a name and an icon. You can
-            always change it later.
-          </DialogDescription>
         </DialogHeader>
-        Invite Modal
+        <div className="p-6">
+          <Label
+            className="fontbold text-xs uppercase text-zinc-500
+            dark:text-secondary/70"
+          >
+            Server invite link
+          </Label>
+          <div className="mt-2 flex items-center gap-x-2">
+            <Input
+              className="border-0 bg-zinc-300/50 text-black 
+              focus-visible:ring-0 focus-visible:ring-offset-0"
+              value={inviteUrl}
+              readOnly
+              disabled={isLoading}
+            />
+            <Button size="icon" onClick={onCopy} disabled={isLoading}>
+              {copied ? (
+                <Check className="h-4 w-4" />
+              ) : (
+                <Copy className="h-4 w-4" />
+              )}
+            </Button>
+          </div>
+          <Button
+            variant="link"
+            size="sm"
+            className="mt-4 text-xs text-zinc-500"
+            onClick={onNewLink}
+            disabled={isLoading}
+          >
+            Generate a new link
+            <RefreshCw className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
