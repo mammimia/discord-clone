9845e96d764990421dd09e98ca25fbcbc7b1c63e
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-05 12:02:09 +0300

    📝 chore(route.ts): add PATCH route for updating server information
    🔒 fix(route.ts): add authorization check to PATCH route to ensure only authorized users can update server information
    
    🔧 fix(route.ts): update server information in the database based on the provided serverId and profileId
    
    🐛 fix(route.ts): handle errors and return appropriate responses in case of authorization failure or internal server error

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,34 @@
+import { getCurrentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await getCurrentProfile();
+
+    if (!profile) {
+      return new Response('Not Authorized', { status: 401 });
+    }
+
+    const { name, imageUrl } = await req.json();
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id
+      },
+      data: {
+        name,
+        imageUrl
+      }
+    });
+
+    return NextResponse.json(server, { status: 200 });
+  } catch (error) {
+    console.log('[SERVER_ID_PATCH]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
