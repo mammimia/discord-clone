c175eaec936689e9a601fcf327df46ec99c88ac4
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-05 12:07:49 +0300

    🔨 refactor(modal-provider.tsx): reorganize imports for better readability and maintainability ✨ feat(modal-provider.tsx): add support for managing members with the new ManageMembersModal component 🔧 chore(server-header.tsx): add onClick event handler to the Manage Members dropdown menu item to open the ManageMembersModal 🔧 chore(use-modal-store.ts): add 'manageMembers' as a valid ModalType option for better type safety and clarity

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import CreateServerModal from '@/components/modals/create-server-modal';
-import { useEffect, useState } from 'react';
-import InviteModal from '@/components/modals/invite-modal';
 import EditServerModal from '@/components/modals/edit-server-modal';
+import InviteModal from '@/components/modals/invite-modal';
+import ManageMembersModal from '@/components/modals/manage-members-modal';
+import { useEffect, useState } from 'react';
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -21,6 +22,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <EditServerModal />
       <InviteModal />
+      <ManageMembersModal />
     </>
   );
 };
