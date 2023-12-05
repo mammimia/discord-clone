be208bcbc57005050a5e6726441f12adbfeabff4
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-05 11:13:05 +0300

    📦 feat(route.ts): add PATCH route to update server invite code
    This commit adds a new PATCH route to update the invite code of a server. The route receives a serverId parameter and checks if the current user is authorized. If the user is not authorized, a 401 Unauthorized response is returned. If the serverId is missing, a 400 Bad Request response is returned. Otherwise, the server's invite code is updated with a new UUID and the updated server object is returned in the response body. In case of any errors, a 500 Internal Server Error response is returned.

---
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -0,0 +1,36 @@
+import { getCurrentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await getCurrentProfile();
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse('Server id is missing!', { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id
+      },
+      data: {
+        inviteCode: uuidv4()
+      }
+    });
+
+    return NextResponse.json(server, { status: 200 });
+  } catch (error) {
+    console.log('[SERVER_ID]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
