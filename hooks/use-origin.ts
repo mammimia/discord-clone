40a25f36fec17b6363965313a9cc7692a42ee16b
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-05 10:58:11 +0300

    ✨ feat(use-origin.ts): add custom hook useOrigin to get the current window location origin 🔧 chore(use-origin.ts): refactor useOrigin hook to improve readability and performance

---
+++ hooks/use-origin.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+export const useOrigin = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const origin =
+    typeof window !== 'undefined' && mounted ? window.location.origin : '';
+
+  if (!mounted) return '';
+
+  return origin;
+};
