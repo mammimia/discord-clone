7863ef96155ccda153f3737bff5a384fd3d1c585
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-06 14:20:50 +0300

    🔨 refactor(modal-provider.tsx): import and add CreateChannelModal component to ModalProvider to enable creating channels 🔧 fix(server-header.tsx): add onClick event handler to Create Channel dropdown menu item to open the createChannel modal 🔧 fix(use-modal-store.ts): add 'createChannel' to ModalType enum to handle opening the createChannel modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -77,7 +77,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
+          <DropdownMenuItem
+            className="cursor-pointer px-3 py-2 text-sm"
+            onClick={() => onOpen('createChannel')}
+          >
             Create Channel
             <PlusCircle className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
