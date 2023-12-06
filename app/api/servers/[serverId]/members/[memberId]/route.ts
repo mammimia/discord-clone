174bb651df672ec2c5b8b8c82ca16eea63f46eaa
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-06 14:05:31 +0300

    🔧 fix(route.ts): add DELETE function to handle deleting a member from a server 🔒 refactor(route.ts): add authorization check to DELETE function to ensure only authorized users can delete members 🐛 fix(route.ts): fix bug where serverId and memberId were not being checked for existence in DELETE function 🔧 fix(route.ts): update DELETE function to use the correct syntax for deleting a member from a server in the database ✨ feat(route.ts): add include statement to DELETE function to include the updated list of members after deletion 🔧 fix(route.ts): handle error in DELETE function and return appropriate response

--- app/api/servers/[serverId]/members/[memberId]/route.ts
+++ app/api/servers/[serverId]/members/[memberId]/route.ts
@@ -49,3 +49,50 @@ export async function PATCH(
     });
   }
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { memberId: string; serverId: string } }
+) {
+  try {
+    const profile = await getCurrentProfile();
+    const { serverId, memberId } = params;
+
+    if (!profile) return new NextResponse('Unauthorized', { status: 401 });
+
+    if (!serverId || !memberId)
+      return new NextResponse('Bad Request', { status: 400 });
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id
+      },
+      data: {
+        members: {
+          deleteMany: {
+            id: memberId,
+            profileId: {
+              not: profile.id
+            }
+          }
+        }
+      },
+      include: {
+        members: {
+          include: { profile: true },
+          orderBy: {
+            role: 'asc'
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server, { status: 200 });
+  } catch (error) {
+    console.log('[MEMBERS_ID_DELETE]', error);
+    return new NextResponse('Internal Error', {
+      status: 500
+    });
+  }
+}
