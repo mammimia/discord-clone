7c96c1d15c918e70b5d8b281c3832a45987ca935
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-06 13:59:32 +0300

    📝 chore(route.ts): add PATCH method to update member role in server 🐛 fix(route.ts): fix error handling and response status codes for PATCH method in updating member role in server

---
+++ app/api/servers/[serverId]/members/[memberId]/route.ts
@@ -0,0 +1,51 @@
+import { getCurrentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { memberId: string; serverId: string } }
+) {
+  try {
+    const profile = await getCurrentProfile();
+    const { serverId, memberId } = params;
+    const { role } = await req.json();
+
+    if (!profile) return new NextResponse('Unauthorized', { status: 401 });
+
+    if (!serverId || !memberId || !role)
+      return new NextResponse('Bad Request', { status: 400 });
+
+    const server = await db.server.update({
+      where: { id: serverId, profileId: profile.id },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: memberId,
+              profileId: {
+                not: profile.id
+              }
+            },
+            data: { role }
+          }
+        }
+      },
+      include: {
+        members: {
+          include: { profile: true },
+          orderBy: {
+            role: 'asc'
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server, { status: 200 });
+  } catch (error) {
+    console.log('[MEMBERS_ID_PATCH]', error);
+    return new NextResponse('Internal Error', {
+      status: 500
+    });
+  }
+}
