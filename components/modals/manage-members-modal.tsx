cadd3c437ad29afc0ce687417f1c5e2a8450406a
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-06 10:31:30 +0300

    🎨 style(manage-members-modal.tsx): improve layout and styling of the Manage Members modal 🔧 refactor(manage-members-modal.tsx): refactor code to use ScrollArea component for better scrolling experience 🚀 feat(manage-members-modal.tsx): display member avatars, names, roles, and emails in the Manage Members modal 🐛 fix(manage-members-modal.tsx): fix missing imports and variables in the Manage Members modal

--- components/modals/manage-members-modal.tsx
+++ components/modals/manage-members-modal.tsx
@@ -7,25 +7,50 @@ import {
   DialogHeader,
   DialogTitle
 } from '@/components/ui/dialog';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { useModal } from '@/hooks/use-modal-store';
+import { ServerWithMembersWithProfiles } from '@/types';
+import { ShieldAlert, ShieldCheck } from 'lucide-react';
+import UserAvatar from '../ui/user-avatar';
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="ml-2 h-4 w-4 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="ml-2 h-4 w-4 text-rose-500" />
+};
 
 const ManageMembersModal = () => {
   const { isOpen, onOpen, onClose, type, data } = useModal();
 
   const isModalOpen = type === 'manageMembers' && isOpen;
 
+  const server = data?.server as ServerWithMembersWithProfiles;
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+      <DialogContent className="overflow-hidden bg-white  text-black">
         <DialogHeader className="px-6 pt-8">
           <DialogTitle className="text-center text-2xl font-bold">
             Manage Members
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} Members
+          </DialogDescription>
         </DialogHeader>
-        <DialogDescription className="text-center text-zinc-500">
-          Manage members and roles for your server.
-        </DialogDescription>
-        <div className="p-6"></div>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div key={member.id} className="mb-6 flex items-center gap-x-2">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="flex items-center text-xs font-semibold">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
+              </div>
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
