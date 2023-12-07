e2f3a3eb4e20bdadbcebba68f38d85519dcf03cc
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-07 16:56:51 +0300

    ✨ feat(delete-server-modal.tsx): add DeleteServerModal component to handle server deletion functionality in the UI

---
+++ components/modals/delete-server-modal.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog';
+import { useModal } from '@/hooks/use-modal-store';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const DeleteServerModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isModalOpen = type === 'deleteServer' && isOpen;
+
+  const onConfirm = async () => {
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/servers/${data?.server?.id}/delete`);
+
+      onClose();
+      router.refresh();
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl font-bold">
+            Delete Server
+          </DialogTitle>
+          <DialogDescription className="text-centers text-zinc-500">
+            Are you sure you want to delete{' '}
+            <span className="font-semibold text-indigo-500">
+              {data?.server?.name}
+            </span>{' '}
+            server?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex w-full items-center justify-between">
+            <Button disabled={isLoading} variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button disabled={isLoading} variant="primary" onClick={onConfirm}>
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeleteServerModal;
