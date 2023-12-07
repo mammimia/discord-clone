8c282f8d024c37f768e2331496620a562403540c
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-07 16:53:41 +0300

    🐛 fix(leave-server-modal.tsx): fix incorrect import of useRouter from 'next/navigation' to 'next/router' to resolve compilation error ✨ feat(leave-server-modal.tsx): add functionality to send a PATCH request to leave the server and refresh the page after successful leave to reflect the updated server list

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -10,13 +10,31 @@ import {
   DialogTitle
 } from '@/components/ui/dialog';
 import { useModal } from '@/hooks/use-modal-store';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const LeaveServerModal = () => {
   const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const isModalOpen = type === 'leaveServer' && isOpen;
+
+  const onConfirm = async () => {
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/servers/${data?.server?.id}/leave`);
+
+      onClose();
+      router.refresh();
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -35,10 +53,10 @@ const LeaveServerModal = () => {
         </DialogHeader>
         <DialogFooter className="bg-gray-100 px-6 py-4">
           <div className="flex w-full items-center justify-between">
-            <Button disabled={isLoading} variant="ghost" onClick={() => {}}>
+            <Button disabled={isLoading} variant="ghost" onClick={onClose}>
               Cancel
             </Button>
-            <Button disabled={isLoading} variant="primary" onClick={() => {}}>
+            <Button disabled={isLoading} variant="primary" onClick={onConfirm}>
               Confirm
             </Button>
           </div>
