9cc2c0d5778fb23dd80ede147eaa66fd77529b0e
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-08 08:20:25 +0300

    🔧 chore(action-tooltip.tsx): make align prop optional to allow for more flexible usage of the component

--- components/action-tooltip.tsx
+++ components/action-tooltip.tsx
@@ -11,7 +11,7 @@ interface ActionTooltipProps {
   label: string;
   children: React.ReactNode;
   side: 'top' | 'bottom' | 'left' | 'right';
-  align: 'start' | 'center' | 'end';
+  align?: 'start' | 'center' | 'end';
 }
 
 const ActionTooltip = ({
