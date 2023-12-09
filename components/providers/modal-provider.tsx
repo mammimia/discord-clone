76135429c2c40fd214abe94c434612850d4bf773
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-09 21:32:40 +0300

    🔀 chore(modal-provider.tsx): reorganize import statements for better readability and maintainability 🔀 chore(modal-provider.tsx): add import statement for DeleteChannelModal to ensure it is available for use

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,12 +2,13 @@
 
 import CreateChannelModal from '@/components/modals/create-channel-modal';
 import CreateServerModal from '@/components/modals/create-server-modal';
+import DeleteChannelModal from '@/components/modals/delete-channel-modal';
+import DeleteServerModal from '@/components/modals/delete-server-modal';
 import EditServerModal from '@/components/modals/edit-server-modal';
 import InviteModal from '@/components/modals/invite-modal';
+import LeaveServerModal from '@/components/modals/leave-server-modal';
 import ManageMembersModal from '@/components/modals/manage-members-modal';
 import { useEffect, useState } from 'react';
-import LeaveServerModal from '../modals/leave-server-modal';
-import DeleteServerModal from '../modals/delete-server-modal';
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -29,6 +30,7 @@ const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
     </>
   );
 };
