d63addb829eb6807ed962e7712c741d80773f1dd
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-09 21:48:56 +0300

    🔧 fix(route.ts): add PATCH route to update channel name and type 🔒 chore(route.ts): add authorization check to PATCH route to ensure user is authorized to update channel 🔒 chore(route.ts): add validation checks to PATCH route to ensure required parameters are provided 🔒 chore(route.ts): add validation check to PATCH route to disallow 'general' as channel name 🔧 fix(route.ts): update server channel name and type in PATCH route 🔧 fix(route.ts): handle error in PATCH route and return Internal Server Error response

--- app/api/servers/[serverId]/channels/[channelId]/route.ts
+++ app/api/servers/[serverId]/channels/[channelId]/route.ts
@@ -48,3 +48,60 @@ export async function DELETE(
     return new NextResponse('Internal Server Error', { status: 500 });
   }
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string; channelId: string } }
+) {
+  try {
+    const profile = await getCurrentProfile();
+    const { name, type } = await req.json();
+
+    if (!profile) {
+      return new Response('Not Authorized', { status: 401 });
+    }
+
+    if (!params?.serverId || !params?.channelId || !name || !type) {
+      return new Response('Bad Request', { status: 400 });
+    }
+
+    if (name === 'general') {
+      return new Response('Name cannot be general', { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+            }
+          }
+        }
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              name: {
+                not: 'general'
+              }
+            },
+            data: {
+              name,
+              type
+            }
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server, { status: 200 });
+  } catch (error) {
+    console.log('[CHANNEL_ID_PATCH]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
