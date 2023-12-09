8a34f15c4d80677a9f9801f73735217c22f96792
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-09 21:32:28 +0300

    ✨ feat(delete-channel-modal.tsx): add DeleteChannelModal component to handle deletion of channels
    The DeleteChannelModal component is responsible for displaying a modal dialog to confirm the deletion of a channel. It uses the useModal hook to manage the modal state and the useRouter hook to navigate to the appropriate page after deletion.
    
    The component includes a confirmation message with the name of the channel to be deleted and two buttons: Cancel and Confirm. The Cancel button closes the modal, while the Confirm button triggers the deletion process. The deletion is performed by making a DELETE request to the server API endpoint and then refreshing the page and navigating to the server's page.
    
    The component also handles loading state by disabling the buttons and displaying a loading indicator while the deletion is in progress.

---
+++ components/modals/delete-channel-modal.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog';
+import { useModal } from '@/hooks/use-modal-store';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const DeleteChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isModalOpen = type === 'deleteChannel' && isOpen;
+
+  const onConfirm = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(
+        `/api/servers/${data?.server?.id}/channels/${data?.channel?.id}`
+      );
+
+      onClose();
+      router.refresh();
+      router.push(`/servers/${data?.server?.id}`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl font-bold">
+            Delete Channel
+          </DialogTitle>
+          <DialogDescription className="text-centers text-zinc-500">
+            Are you sure you want to do this ? <br />
+            <span className="font-semibold text-indigo-500">
+              #{data?.channel?.name}
+            </span>{' '}
+            will be deleted permanently.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex w-full items-center justify-between">
+            <Button disabled={isLoading} variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button disabled={isLoading} variant="primary" onClick={onConfirm}>
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeleteChannelModal;
