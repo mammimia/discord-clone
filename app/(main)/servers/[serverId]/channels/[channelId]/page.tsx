aa70aee738ef34380f79833a6a873160939c7468
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-10 10:34:14 +0300

    🔀 refactor(page.tsx): refactor ChannelIdPage component to improve readability and maintainability 🔒 chore(page.tsx): add authentication and authorization logic to ChannelIdPage component to ensure only authenticated users with proper permissions can access the page 🔧 chore(page.tsx): add error handling logic to ChannelIdPage component to handle cases where the channel or member data is not found 🎨 style(page.tsx): format ChannelIdPage component code for better consistency and readability

--- app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,46 @@
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+import ChatHeader from '@/components/chat/chat-header';
+import { getCurrentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await getCurrentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+      serverId: params.serverId
+    }
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id
+    }
+  });
+
+  if (!channel || !member) return redirect('/');
+
+  return (
+    <div className="flex h-full flex-col bg-white dark:bg-[#313338]">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
