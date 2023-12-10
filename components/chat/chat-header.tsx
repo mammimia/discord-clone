a3e04841b4a3ca0f39fe2da6b945c5a373531213
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-10 10:33:59 +0300

    ✨ feat(chat-header.tsx): add ChatHeader component to display the header of a chat with serverId, name, type, and optional imageUrl

---
+++ components/chat/chat-header.tsx
@@ -0,0 +1,22 @@
+import { Hash, Menu } from 'lucide-react';
+
+interface ChatHeaderProps {
+  serverId: string;
+  name: string;
+  type: 'channel' | 'conversation';
+  imageUrl?: string;
+}
+
+const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
+  return (
+    <div className="text-md flex h-12 items-center border-b-2 border-neutral-200 px-3 font-semibold dark:border-neutral-800">
+      <Menu className="block md:hidden" />
+      {type === 'channel' && (
+        <Hash className="mx-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      )}
+      <p className="text-md font-semibold text-black dark:text-white">{name}</p>
+    </div>
+  );
+};
+
+export default ChatHeader;
