7c63550342943bf4f0dd1ea4b8d8ae6a19831dde
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-11 11:06:29 +0300

    🔀 refactor(chat-header.tsx): update import paths for UserAvatar and SocketIndicator components to use absolute paths for better maintainability 🎨 style(chat-header.tsx): add SocketIndicator component to display socket connection status in the chat header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import MobileToggle from '@/components/mobile-toggle';
 import { Hash } from 'lucide-react';
-import UserAvatar from '../ui/user-avatar';
+import UserAvatar from '@/components/ui/user-avatar';
+import SocketIndicator from '@/components/socket-indicator';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -20,6 +21,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="text-md font-semibold text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
