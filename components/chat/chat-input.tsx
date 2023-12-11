0904c862bc5bf8dc373c19ce0fe8bfb50d585e6e
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-11 11:23:51 +0300

    🎨 style(chat-input.tsx): format code for better readability and consistency
    🔧 chore(chat-input.tsx): import missing Input component from '@/components/ui/input'
    
    🚀 feat(chat-input.tsx): add smiley icon button for emoji picker
    
    🐛 fix(chat-input.tsx): fix button alignment issue and adjust styles for input field

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Plus } from 'lucide-react';
+import { Plus, Smile } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -45,11 +46,24 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     type="button"
                     onClick={() => {}}
                     className="absolute left-8 top-7 flex h-[24px] w-[24px] items-center
-                    justify-center rounded-full bg-zinc-500 p-1 hover:bg-zinc-600
-                dark:bg-zinc-400 dark:hover:bg-zinc-300"
+                    justify-center rounded-full bg-zinc-500 p-1 hover:bg-zinc-600 
+                    dark:bg-zinc-400 dark:hover:bg-zinc-300"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
                   </button>
+                  <Input
+                    disabled={isLoading}
+                    className="border-0 border-none bg-zinc-200/90 px-14 py-6
+                    text-zinc-600 focus-visible:ring-0 focus-visible:ring-offset-0
+                    dark:bg-zinc-700/75 dark:text-zinc-200"
+                    placeholder={`Message to ${
+                      type === 'conversation' ? name : '#' + name
+                    }`}
+                    {...field}
+                  />
+                  <div className="absolute right-8 top-7">
+                    <Smile />
+                  </div>
                 </div>
               </FormControl>
             </FormItem>
