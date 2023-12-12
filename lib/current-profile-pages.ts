ed69db7ead3ecf976eab71e6a73d4338678e55df
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-12 07:47:52 +0300

    📦 chore(current-profile-pages.ts): add getCurrentProfile function to retrieve the current user's profile from the database
    The getCurrentProfile function is added to the current-profile-pages.ts file. This function uses the getAuth function from the @clerk/nextjs/server package to retrieve the userId from the request. If the userId is not available, the function returns null. Otherwise, it uses the userId to query the database and retrieve the corresponding profile using the db.profile.findUnique method. The retrieved profile is then returned by the function.

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,15 @@
+import { db } from '@/lib/db';
+import { getAuth } from '@clerk/nextjs/server';
+import { NextApiRequest } from 'next';
+
+export const getCurrentProfile = async (req: NextApiRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) return null;
+
+  const profile = await db.profile.findUnique({
+    where: { userId }
+  });
+
+  return profile;
+};
