748e10089c055459378ba56034c9157d93dd22cf
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-12 08:24:10 +0300

    ✨ feat(message-file-modal.tsx): add MessageFileModal component to handle adding attachments to messages
    The MessageFileModal component is responsible for handling the addition of attachments to messages. It provides a form for users to input the URL of the attachment and submit it. The component uses the FileUpload component to handle the actual file upload process.
    
    The form input is validated using the zod library, ensuring that the URL is at least 2 characters long and a valid URL. Upon submission, the form data is sent to the server using an axios POST request. If the request is successful, the form is reset, the router is refreshed, and the modal is closed. If there is an error, it is logged to the console.
    
    The component is used within a Dialog component to display the form as a modal. The modal includes a title and description, providing guidance to the user. The form includes a file upload input field and a submit button.

---
+++ components/modals/message-file-modal.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import FileUpload from '@/components/file-upload';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from '@/components/ui/form';
+import { useModal } from '@/hooks/use-modal-store';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import queryString from 'query-string';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  fileUrl: z
+    .string()
+    .min(2, {
+      message: 'Attachment URL must be at least 2 characters long'
+    })
+    .url({
+      message: 'Attachment URL must be a valid URL'
+    })
+});
+
+const MessageFileModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === 'messageFile';
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      fileUrl: ''
+    }
+  });
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = queryString.stringifyUrl({
+        url: data?.apiUrl || '',
+        query: data?.query
+      });
+      await axios.post(url, { ...values, content: values.fileUrl });
+
+      form.reset();
+      router.refresh();
+      handleClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl font-bold">
+            Add an attachment
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Send a file as a message
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="fileUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="messageFile"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button disabled={isLoading} variant="primary">
+                Send
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MessageFileModal;
