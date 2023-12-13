5c346dba57e054a14551264ff858c0d0fce45721
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-13 07:57:14 +0300

    🚀 feat(page.tsx): add ChatMessages component to display chat messages in the channel page 🎉 feat(chat-messages.tsx): create ChatMessages component to render chat messages in the channel page

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import ChatWelcome from '@/components/chat/chat-welcome';
+import { Member } from '@prisma/client';
+
+interface ChatMessagesProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: 'channelId' | 'conversationId';
+  paramValue: string;
+  type: 'channel' | 'conversation';
+}
+
+const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type
+}: ChatMessagesProps) => {
+  return (
+    <div className="flex flex-1 flex-col overflow-y-auto py-4">
+      <div className="flex-1" />
+      <ChatWelcome type={type} name={name} />
+    </div>
+  );
+};
+
+export default ChatMessages;
