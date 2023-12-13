34f6090079bc140f2e5ef23437dc2011b5e063ea
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-13 08:13:30 +0300

    🔧 fix(chat-messages.tsx): import missing dependencies to fix compilation errors ✨ feat(chat-messages.tsx): add loading and error states to display appropriate UI feedback to the user

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import ChatWelcome from '@/components/chat/chat-welcome';
+import { useChatQuery } from '@/hooks/use-chat-query';
 import { Member } from '@prisma/client';
+import { Loader2, ServerCrash } from 'lucide-react';
 
 interface ChatMessagesProps {
   name: string;
@@ -26,6 +28,36 @@ const ChatMessages = ({
   paramValue,
   type
 }: ChatMessagesProps) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey: `chat:${chatId}`,
+      apiUrl,
+      paramKey,
+      paramValue
+    });
+
+  if (status === 'pending') {
+    return (
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <Loader2 className="my-4 h-7 w-7 animate-spin text-zinc-500" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === 'error') {
+    return (
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <ServerCrash className="my-4 h-7 w-7 text-zinc-500" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong...
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-1 flex-col overflow-y-auto py-4">
       <div className="flex-1" />
