d6a9afd09d03cc65f536520e9d87b7806eb711c6
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-16 12:01:51 +0300

    📦 chore(direct-messages): add API endpoint for deleting and updating direct messages
    🔒 fix(direct-messages): add authorization checks to prevent unauthorized access to direct messages
    
    🔍 fix(direct-messages): add error handling for various scenarios such as missing profile, conversation, or direct message
    
    🔧 fix(direct-messages): update direct message content and mark as deleted when deleting a message
    
    🔧 fix(direct-messages): update direct message content when editing a message
    
    📡 feat(direct-messages): emit socket event to update messages in real-time after deleting or editing a direct message
    
    🐛 fix(direct-messages): handle and log internal errors

---
+++ pages/api/socket/direct-messages/[directMessageId].ts
@@ -0,0 +1,133 @@
+import { getCurrentProfile } from '@/lib/current-profile-pages';
+import { db } from '@/lib/db';
+import { NextApiResponseServerIo } from '@/types';
+import { NextApiRequest } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  if (req.method !== 'DELETE' && req.method !== 'PATCH') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const profile = await getCurrentProfile(req);
+    const { directMessageId, conversationId } = req.query;
+    const { content } = req.body;
+
+    if (!profile) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    if (!directMessageId || !conversationId) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    const conversation = await db.conversation.findUnique({
+      where: {
+        id: conversationId as string,
+        OR: [
+          {
+            memberOne: {
+              profileId: profile.id
+            }
+          },
+          {
+            memberTwo: {
+              profileId: profile.id
+            }
+          }
+        ]
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true
+          }
+        },
+        memberTwo: {
+          include: {
+            profile: true
+          }
+        }
+      }
+    });
+
+    if (!conversation) {
+      return res.status(404).json({ error: 'Conversation not found' });
+    }
+
+    const member =
+      conversation.memberOne.profileId === profile.id
+        ? conversation.memberOne
+        : conversation.memberTwo;
+
+    if (!member) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    let directMessage = await db.directMessage.findFirst({
+      where: {
+        id: directMessageId as string,
+        conversationId: conversationId as string
+      },
+      include: {
+        member: {
+          include: { profile: true }
+        }
+      }
+    });
+
+    if (!directMessage || directMessage.deleted) {
+      return res.status(404).json({ error: 'Direct Message not found' });
+    }
+
+    const isMessageOwner = directMessage.member.profileId === profile.id;
+    const isAdmin = member.role === 'ADMIN';
+    const isModerator = member.role === 'MODERATOR';
+    const canModify = isMessageOwner || isAdmin || isModerator;
+
+    if (!canModify) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    if (req.method === 'DELETE') {
+      directMessage = await db.directMessage.update({
+        where: { id: directMessage.id },
+        data: {
+          deleted: true,
+          fileUrl: null,
+          content: 'This message has been deleted.'
+        },
+        include: { member: { include: { profile: true } } }
+      });
+    }
+
+    if (req.method === 'PATCH') {
+      if (!isMessageOwner) {
+        return res.status(403).json({ error: 'Forbidden' });
+      }
+
+      if (!content) {
+        return res.status(400).json({ error: 'Bad Request' });
+      }
+
+      directMessage = await db.directMessage.update({
+        where: { id: directMessage.id },
+        data: {
+          content
+        },
+        include: { member: { include: { profile: true } } }
+      });
+    }
+
+    const updateKey = `chat:${conversationId}:messages:update`;
+    res?.socket?.server?.io?.emit(updateKey, directMessage);
+
+    return res.status(200).json(directMessage);
+  } catch (error) {
+    console.log('[MESSAGE_ID]', error);
+    return res.status(500).json({ error: 'Internal Error' });
+  }
+}
