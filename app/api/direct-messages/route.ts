18c78e8a8ddfd1b28f74e4f53f4a702664ace593
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-16 11:42:13 +0300

    📦 chore(route.ts): add route for retrieving direct messages 🔒 feat(route.ts): add authentication check to ensure only authorized users can access direct messages 🔒 feat(route.ts): add validation check for conversationId parameter to ensure it is provided 🔍 feat(route.ts): add support for pagination using cursor-based pagination 🔍 feat(route.ts): retrieve direct messages based on conversationId and cursor 🔍 feat(route.ts): include member profile information in the retrieved direct messages 🔍 feat(route.ts): order retrieved direct messages by createdAt in descending order 🔍 feat(route.ts): return retrieved direct messages and nextCursor in the response 🔍 feat(route.ts): set nextCursor to the id of the last message if there are more messages available 🔧 fix(route.ts): handle errors and return appropriate response in case of any error during retrieval of direct messages

---
+++ app/api/direct-messages/route.ts
@@ -0,0 +1,77 @@
+import { getCurrentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { DirectMessage } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+const MESSAGES_BATCH = 10;
+
+export async function GET(req: Request) {
+  try {
+    const profile = await getCurrentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const cursor = searchParams.get('cursor');
+    const conversationId = searchParams.get('conversationId');
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!conversationId) {
+      return new NextResponse('Bad Request', { status: 400 });
+    }
+
+    let messages: DirectMessage[] = [];
+
+    if (cursor) {
+      messages = await db.directMessage.findMany({
+        take: MESSAGES_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor
+        },
+        where: {
+          conversationId
+        },
+        include: {
+          member: {
+            include: {
+              profile: true
+            }
+          }
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      });
+    } else {
+      messages = await db.directMessage.findMany({
+        take: MESSAGES_BATCH,
+        where: {
+          conversationId
+        },
+        include: {
+          member: {
+            include: {
+              profile: true
+            }
+          }
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      });
+    }
+
+    let nextCursor = null;
+
+    if (messages.length === MESSAGES_BATCH) {
+      nextCursor = messages[messages.length - 1].id;
+    }
+
+    return NextResponse.json({ items: messages, nextCursor }, { status: 200 });
+  } catch (error) {
+    console.log('[DIRECT_MESSAGES_GET]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
