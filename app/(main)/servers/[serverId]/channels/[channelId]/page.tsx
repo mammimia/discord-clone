8bae4256c3b1fa19c696fe0d25a848ea6ab4522f
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-17 20:17:46 +0300

    🔧 fix(page.tsx): import MediaRoom component to enable usage of media rooms in channels 🚀 feat(page.tsx): add support for different channel types (text, audio, video) to render appropriate components based on channel type

--- app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,11 @@
 import ChatHeader from '@/components/chat/chat-header';
 import ChatInput from '@/components/chat/chat-input';
 import ChatMessages from '@/components/chat/chat-messages';
+import MediaRoom from '@/components/media-room';
 import { getCurrentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirectToSignIn } from '@clerk/nextjs';
+import { ChannelType } from '@prisma/client';
 import { redirect } from 'next/navigation';
 
 interface ChannelIdPageProps {
@@ -41,29 +43,39 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={channel.serverId}
         type="channel"
       />
-      <ChatMessages
-        member={member}
-        name={channel.name}
-        chatId={channel.id}
-        type="channel"
-        apiUrl="/api/messages"
-        socketUrl="/api/socket/messages"
-        socketQuery={{
-          serverId: channel.serverId,
-          channelId: channel.id
-        }}
-        paramKey="channelId"
-        paramValue={channel.id}
-      />
-      <ChatInput
-        name={channel.name}
-        type="channel"
-        apiUrl="/api/socket/messages"
-        query={{
-          serverId: channel.serverId,
-          channelId: channel.id
-        }}
-      />
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <ChatMessages
+            member={member}
+            name={channel.name}
+            chatId={channel.id}
+            type="channel"
+            apiUrl="/api/messages"
+            socketUrl="/api/socket/messages"
+            socketQuery={{
+              serverId: channel.serverId,
+              channelId: channel.id
+            }}
+            paramKey="channelId"
+            paramValue={channel.id}
+          />
+          <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{
+              serverId: channel.serverId,
+              channelId: channel.id
+            }}
+          />
+        </>
+      )}
+      {channel.type === ChannelType.AUDIO && (
+        <MediaRoom chatId={channel.id} video={false} audio={true} />
+      )}
+      {channel.type === ChannelType.VIDEO && (
+        <MediaRoom chatId={channel.id} video={true} audio={false} />
+      )}
     </div>
   );
 };
