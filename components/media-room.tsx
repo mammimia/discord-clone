8a719517bb646a25f0c5c6f8775df9acd354c890
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-12-17 20:15:16 +0300

    ✨ feat(media-room.tsx): add MediaRoom component to handle video conferencing functionality
    The MediaRoom component is responsible for handling video conferencing functionality. It uses the LiveKitRoom and VideoConference components from the @livekit/components-react library.
    
    The component takes in the following props:
    - chatId: the ID of the chat room
    - video: a boolean indicating whether video is enabled
    - audio: a boolean indicating whether audio is enabled
    
    Inside the component, it uses the useUser hook from the @clerk/nextjs library to get the current user. It then fetches a token from the /api/livekit endpoint using the chatId and the user's name. The token is used to authenticate the user in the video conference.
    
    If the token is not available, a loading spinner is displayed. Once the token is available, the LiveKitRoom component is rendered with the appropriate props.
    
    The MediaRoom component is exported as the default export.

---
+++ components/media-room.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { LiveKitRoom, VideoConference } from '@livekit/components-react';
+import '@livekit/components-styles';
+import { Channel } from '@prisma/client';
+import { useUser } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
+
+interface MediaRoomProps {
+  chatId: string;
+  video: boolean;
+  audio: boolean;
+}
+
+const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
+  const { user } = useUser();
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    if (!user || !user.firstName || !user.lastName) return;
+
+    const name = `${user.firstName} ${user.lastName}`;
+    async () => {
+      try {
+        const response = await fetch(
+          `/api/livekit?room=${chatId}&username=${name}`
+        );
+        const data = await response.json();
+        setToken(data.token);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  }, [user, chatId]);
+
+  if (!token) {
+    return (
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <Loader2 className="my-4 h-7 w-7 animate-spin text-zinc-500" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <LiveKitRoom
+      data-lk-theme="default"
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_SERVER_URL}
+      token={token}
+      connect={true}
+      audio={audio}
+      video={video}
+    >
+      <VideoConference />
+    </LiveKitRoom>
+  );
+};
+
+export default MediaRoom;
